a4960fc533a04ba8dc1c0172c804aadb7548c0bf
spawn1978 <leguizamoncarlos@gmail.com>
2020-07-07 01:20:58 -0300

    Rearrange text Add new token name

--- app/src/main/java/com/example/api2/GenerateToken.java
+++ app/src/main/java/com/example/api2/GenerateToken.java
@@ -22,4 +22,4 @@ public class GenerateToken extends AppCompatActivity {
     }
 
 
-    }
+}
--- app/src/main/java/com/example/api2/MainActivity.java
+++ app/src/main/java/com/example/api2/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.api2;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+
+import java.security.SecureRandom;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,13 +17,41 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void act_generate( View view){
+    public void act_generate(View view) {
         Intent act_generate = new Intent(this, GenerateToken.class);
         startActivity(act_generate);
     }
 
-    public void add_newtoken( View view){
+    public void add_newtoken(View view) {
         Intent add_newtoken = new Intent(this, AddNewTokenName.class);
         startActivity(add_newtoken);
     }
+/*
+    public class GenerateToken {
+        public static final String CHARACTERS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        public static final int SECURE_TOKEN_LENGTH = 64;
+        private static final SecureRandom random = new SecureRandom();
+        private static final char[] symbols = CHARACTERS.toCharArray();
+        private static final char[] buf = new char[SECURE_TOKEN_LENGTH];
+        private  final SecureRandom random = new SecureRandom();
+        private  final char[] symbols = CHARACTERS.toCharArray();
+        private  final char[] buf = new char[SECURE_TOKEN_LENGTH];
+        public  String nextToken() {
+            for (int idx = 0; idx < buf.length; ++idx)
+                buf[idx] = symbols[random.nextInt(symbols.length)];
+            return new String(buf);
+        }
+    }
+*/
+    //package com.timebuddy.gae.server.service;
+    public class GeneratorToken {
+        protected  SecureRandom random = new SecureRandom();
+
+        public synchronized String generateToken(String username) {
+            long longToken = Math.abs(random.nextLong());
+            String random = Long.toString(longToken, 16);
+            return (username + ":" + random);
+        }
+    }
 }
+
